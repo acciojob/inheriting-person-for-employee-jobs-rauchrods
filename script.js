1b3b94dfffc2853c219a0b21dd7b11c588ba05f0
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-04-09 11:09:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // complete this js code
-function Person(name, age) {}
+Person.prototype.greet = function(){
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
+Employee.prototype.jobGreet = function(){
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+}
+function Person(name, age) {
+	this.name=name;
+    this.age=age;
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	Person.call(this,name,age);
+    this.jobTitle = jobTitle;
+}
+
+
 
 // Do not change code below this line
 window.Person = Person;
